5d5438447c8292e721b30753b65f714be3296943
bhuse <bhuse@iu.edu>
2019-09-26 18:30:16 -0400

    send it

---
+++ Labs/Week5/js/app.js
@@ -0,0 +1,30 @@
+Vue.component("student-card", {
+    props: [ "student" ],
+    template: "<div class='student' v-bind:class='{ cardActive:isactive }'>{{ student.name }} : {{ student.skill }}</div>"
+})
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        students: [
+            { name: "Sienna", skill: 2, joy: 0 },
+            { name: "Cyan", skill: 0, joy: 5 },
+            { name: "Magenta", skill: 3, joy: 3 }
+        ],
+        currentStudent: { name: "Sienna", skill: 2, joy: 0 },
+        curStudentId: 0,
+        cardActive: true
+    },
+    methods: {
+        arrowClicked: function() {
+            this.curStudentId ++;
+            this.currentStudent = this.students[this.curStudentId];
+
+            if(this.curStudentId >= this.students.length-1) {
+                this.curStudentId = -1;
+            }
+
+            this.cardActive = !this.cardAcive;
+        }
+    }
+})
